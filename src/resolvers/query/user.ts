a4babe94df19d3940e7f86022c03a87bc16f8de7
KBJ-ID <ideasdeveloperskbj@gmail.com>
2021-05-29 09:30:08 -0500

    Paginación en géneros y users
    Funciones generales de paginacion en los servicios de usuarios y generos respectivamente.
    
    Se agregaron parametros de paginacion(page, ItemsPage, total, pages, skip ) al query de usuarios y generos respectivamente de los esquemas de graphql.

--- src/resolvers/query/user.ts
+++ src/resolvers/query/user.ts
@@ -1,14 +1,10 @@
-import { findOneElement, findElements } from './../../lib/db-operations';
-import { EXPIRETIME, MESSAGES } from './../../config/constants';
 import { IResolvers } from 'graphql-tools';
-import { COLLECTIONS } from './../../config/constants';
-import JWT from './../../lib/jwt';
-import bcrypt from 'bcrypt';
 import UsersService from '../../services/users.service';
+
 const resolversUserQuery: IResolvers = {
   Query: {
-    async users(_, __, context) {
-      return new UsersService(_, __, context).items();
+    async users(_, {page, itemsPage}, context) {
+      return new UsersService(_, {pagination:{ page, itemsPage}}, context).items();
     },
 
     async login(_, { email, password }, context) {
